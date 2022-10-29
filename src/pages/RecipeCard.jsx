f754967f021056d8afb1afe01f4d57c3a22a3d29
qxan <mehmet.enfa@gmail.com>
2022-10-29 21:25:29 +0300

    recipe card ok

--- src/pages/RecipeCard.jsx
+++ src/pages/RecipeCard.jsx
@@ -4,16 +4,17 @@ const RecipeCard = ({ data }) => {
   const navigate = useNavigate();
   console.log(data.recipe);
   return (
-    <div className="flex flex-col justify-center  items-center  mt-5   w-[30rem] text-center">
-      <p className="text-xl mb-5">{data.recipe.label}</p>
+    <div className="flex flex-col justify-center items-center bg-red-700 mt-5 w-[33rem] h-[26rem] text-center text-white gap-3 ml-5 rounded-2xl">
+      <p className="text-3xl mb-2">{data.recipe.label}</p>
+      <p className="w-[22rem] h-[0.3rem] bg-black rounded-xl"></p>
       <img
         src={data.recipe.image}
         alt=""
-        width="150px"
+        width="200px"
         className="rounded-lg"
       />
       <button
-        className="rounded-lg bg-black text-white p-1 w-[5rem] mt-2"
+        className="rounded-xl bg-black text-white p-1 w-[7rem] h-[2.5rem] mt-2"
         onClick={() => navigate("/details", { state: data.recipe })}
       >
         Details
