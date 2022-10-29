134f35aacbc549a44892451fd34c532a9f4b04ab
qxan <mehmet.enfa@gmail.com>
2022-10-29 20:36:18 +0300

    login ok

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import About from "./pages/About";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Details from "./pages/Details";
+import AppRouter from './router/AppRouter'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/details" element={<Details />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <AppRouter />
+    </div>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import reacticon from "../assets/reacticon.png";
 
 const Login = () => {
   const navigate = useNavigate();
   return (
     <div className="bgcontainer flex justify-center items-center ">
-      <div className="  rounded-full h-[25rem] w-[25rem] flex justify-center items-center ">
+      <div className="rounded-full h-[25rem] w-[25rem] flex flex-col justify-center items-center ">
+        <div className="flex flex-col mb-28 text-center">
+          <h1 className="text-6xl text-white mb-5">Recipe App</h1>
+          <p className="w-[25rem] h-[0.3rem] bg-slate-400 rounded-xl"></p>
+        </div>
         <form
           className=" flex flex-col gap-4"
           onSubmit={() => navigate("home")}
         >
           <img
-            src={"https://svgsilh.com/svg/2085075.svg"}
+            src={reacticon}
             alt=""
-            width="200px"
-            className="mx-auto"
+            className="mx-auto w-[20rem]"
             draggable="false"
           />
           <input
             type="text"
             name=""
             id=""
-            placeholder="Your name"
-            className="bg-black text-white p-2 rounded-lg "
+            placeholder="Enter Your Name"
+            className="hover:bg-white hover:text-black bg-black text-white p-2 rounded-xl "
             required
           />
+          <p className="w-[20rem] h-[0.3rem] bg-slate-200 rounded-xl"></p>
           <input
             type="password"
             name=""
             id=""
-            placeholder="Your password"
-            className="bg-black text-white p-2 rounded-lg"
+            placeholder="Enter Your Password"
+            className="hover:bg-white hover:text-black bg-black text-white p-2 rounded-xl"
             required
           />
-          <button className="rounded-lg bg-black text-white w-[5rem] mx-auto p-1">
+          <p className="w-[20rem] h-[0.3rem] bg-slate-200 rounded-xl"></p>
+          <button className="rounded-lg bg-black text-white w-[7rem] mx-auto p-1 hover:bg-cyan-500 hover:text-black hover:scale-125 ">
             Login
           </button>
         </form>
